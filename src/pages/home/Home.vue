2f34da99dc846890ff77e8e5d9a80732a2ddd30c
hueralin <1612415787@qq.com>
2023-07-05 23:09:44 +0800

    feat: add search

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,11 +2,14 @@
   <div>
     <!-- 轮播图 -->
     <carousel/>
+    <!-- 搜索 -->
+    <search/>
   </div>
 </template>
 
 <script lang="ts" setup>
 import Carousel from '@/pages/home/carousel/Carousel.vue'
+import Search from '@/pages/home/search/Search.vue'
 </script>
 
 <style scoped>
---
+++ src/pages/home/search/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search flex justify-center items-center py-5">
+    <el-autocomplete
+        class="mr-1"
+        clearable
+        placeholder="请输入医院名称"
+    />
+    <el-button :icon="Search" type="primary"></el-button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {Search} from '@element-plus/icons-vue'
+</script>
+
+<style lang="scss" scoped>
+.search {
+
+  // 深度选择器，vue 提供的，用于修改组件库的样式
+  :deep(.el-input__wrapper) {
+    width: 400px;
+    height: 40px;
+  }
+
+  :deep(.el-button) {
+    height: 40px;
+  }
+}
+</style>
