7192b262c1be9e83bf698f6fcba03b677c130cc5
hueralin <1612415787@qq.com>
2023-07-06 22:32:27 +0800

    feat: add hospital filter

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,12 +4,21 @@
     <carousel/>
     <!-- 搜索 -->
     <search/>
+    <!-- 展示医院的结构 -->
+    <el-row :gutter="20">
+      <el-col :span="20">
+        <!-- 医院筛选 -->
+        <hospital-filter/>
+      </el-col>
+      <el-col :span="4">right</el-col>
+    </el-row>
   </div>
 </template>
 
 <script lang="ts" setup>
 import Carousel from '@/pages/home/carousel/Carousel.vue'
 import Search from '@/pages/home/search/Search.vue'
+import HospitalFilter from '@/pages/home/hospital-filter/HospitalFilter.vue'
 </script>
 
 <style scoped>
---
+++ src/pages/home/hospital-filter/HospitalFilter.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hospital-filter">
+    <div class="mb-5">医院</div>
+    <div class="flex mb-5">
+      <div class="basis-12 shrink-0">等级：</div>
+      <ul>
+        <li>全部</li>
+        <li>三级甲等</li>
+        <li>三级乙等</li>
+        <li>二级甲等</li>
+        <li>二级乙等</li>
+        <li>一级</li>
+      </ul>
+    </div>
+    <div class="flex mb-5">
+      <div class="basis-12 shrink-0">地域：</div>
+      <ul>
+        <li>全部</li>
+        <li>东城区</li>
+        <li>西城区</li>
+        <li>朝阳区</li>
+        <li>丰台区</li>
+        <li>石景山区</li>
+        <li>海淀区</li>
+        <li>门头沟区</li>
+        <li>房山区</li>
+        <li>大兴区</li>
+        <li>通州区</li>
+        <li>顺义区</li>
+        <li>昌平区</li>
+        <li>怀柔区</li>
+        <li>平谷区</li>
+        <li>密云区</li>
+        <li>延庆区</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.hospital-filter {
+  color: #7f7f7f;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    li {
+      margin-right: 10px;
+      cursor: pointer;
+
+      &:hover {
+        color: #55a6fe;
+      }
+    }
+  }
+}
+</style>
